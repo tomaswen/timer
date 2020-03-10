0b89f3f3b669ee881992e1d44349813ca1eeb658
Tomas Wen Liang <tomas.wenn@gmail.com>
2020-03-10 14:46:47 +0000

    Using process.stdin allows user to beep immediately or set a timer for it

---
+++ timer2.js
@@ -0,0 +1,21 @@
+const stdin = process.stdin;
+// don't worry about these next two lines of setup work.
+stdin.setRawMode(true);
+stdin.setEncoding('utf8');
+
+stdin.on('data', (key) => {
+ if (key === 'b'){
+    process.stdout.write('\x07')
+  }
+  else if (Number(key) >= 1 && Number(key) <= 9) {
+    console.log(`Setting timer for ${key} secondd...`)
+    setTimeout(() => {
+      process.stdout.write('\x07');
+    }, Number(key)*1000);
+  }
+  else if (key === '\u0003') {
+    process.exit();
+  }
+});
+
+console.log('after callback');
