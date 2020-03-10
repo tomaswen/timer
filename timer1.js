a2b618d6f5633c4883c7a801da3dd21406189e86
Tomas Wen Liang <tomas.wenn@gmail.com>
2020-03-10 14:45:43 +0000

    Using CLI arguments creates a timer where it will beep after x seconds

--- timer1.js
+++ timer1.js
@@ -1,7 +1,8 @@
 const time = process.argv.slice(2)
-
 for (let element of time){
-  setTimeout(() => {
+  if (Number(element) >= 0){
+    setTimeout(() => {
     process.stdout.write('\x07');
-  }, Number(element)*1000);
-}
+    }, Number(element)*1000);
+  }
+};
